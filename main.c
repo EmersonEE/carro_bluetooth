c79cddb5f24f3206b10718eb6103c8130071deba
EmersonEE <perezemerson85@gmail.com>
2024-03-20 11:13:34 -0600

    Agregando Librerias

--- main.c
+++ main.c
@@ -1,9 +1,24 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/tm4c123gh6pm.h"
 
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"  //Contiene las API para configurar y manejar los GPIO
+#include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/uart.h"
+#include "utils/uartstdio.h"  //manejo de UART
+#include "utils/uartstdio.c"
+#include "inc/hw_uart.h"
+#include <string.h>
+#include "driverlib/interrupt.h"  //Se proporcionan API para habilitar y
+//habilitar interrupciones, registrar manejadores de interrupciones y
+//establecer la prioridad de las interrupciones.
 
-/**
- * main.c
- */
 int main(void)
 {
 	return 0;
 }
+
