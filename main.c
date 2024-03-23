575bb61f55a88a0756aa57a2489b7a2048707ea8
EmersonAPR <perezemerson85@gmail.com>
2024-03-22 18:47:17 -0600

    Cambiando Proyecto a Windows

--- main.c
+++ main.c
@@ -22,16 +22,16 @@ void Retroceder();
 void Izquierda();
 void Derecha();
 void Stop();
-
+void LeerBluetooth();
 /* HC06 BLUETOOTH
  * TX-->PC6
  * RX-->PC7*/
 
 int LED;
+    unsigned char data;
 
 int main(void)
 {
-    unsigned char data;
 
     SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
 
@@ -39,20 +39,22 @@ int main(void)
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
     GPIOPinConfigure(GPIO_PC6_U3RX);
     GPIOPinConfigure(GPIO_PC7_U3TX);
+
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_UART3);
     GPIOPinTypeGPIOOutput(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7);
-    GPIOPinTypeUART(GPIO_PORTC_BASE, GPIO_PIN_6 | GPIO_PIN_7);
     GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
+
+    GPIOPinTypeUART(GPIO_PORTC_BASE, GPIO_PIN_6 | GPIO_PIN_7);
     UARTConfigSetExpClk(UART3_BASE, SysCtlClockGet(), 9600, (UART_CONFIG_WLEN_8 | UART_CONFIG_PAR_NONE | UART_CONFIG_STOP_ONE));
     UARTEnable(UART3_BASE);
 
     while (1)
     {
-        while (!UARTCharsAvail(UART3_BASE))
-            ;
-        data = UARTCharGetNonBlocking(UART3_BASE);
+
+        LeerBluetooth();
+
         LED = 0;
         if (data == 'F')
         {
@@ -83,7 +85,7 @@ void Adelante()
     LED = 2;
     GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, LED);
     GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7, 9);
-    bluetoothSendMessage("\nTiva send = RED\n");
+    bluetoothSendMessage("\nTiva send = ADELANTE\n");
     SysCtlDelay(100);
 }
 
@@ -92,7 +94,7 @@ void Retroceder()
     LED = 4;
     GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, LED);
     GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7, 6);
-    bluetoothSendMessage("\nTiva send = BLUE\n");
+    bluetoothSendMessage("\nTiva send = REVERSA\n");
     SysCtlDelay(100);
 }
 
@@ -101,7 +103,7 @@ void Derecha()
     LED = 8;
     GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, LED);
     GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7, 5);
-    bluetoothSendMessage("\nTiva send = GREEN\n");
+    bluetoothSendMessage("\nTiva send = DERECHA\n");
     SysCtlDelay(100);
 }
 void Izquierda()
@@ -109,7 +111,7 @@ void Izquierda()
     LED = 14;
     GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, LED);
     GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7, 10);
-    bluetoothSendMessage("\nTiva send = WHITE\n");
+    bluetoothSendMessage("\nTiva send = IZQUIERDA\n");
     SysCtlDelay(100);
 }
 
@@ -118,7 +120,7 @@ void Stop()
     LED = 0;
     GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, LED);
     GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7, LED);
-    bluetoothSendMessage("\nTiva send = NO\n");
+    bluetoothSendMessage("\nTiva send = STOP\n");
     SysCtlDelay(100);
 }
 void bluetoothSendMessage(char *array)
@@ -128,4 +130,10 @@ void bluetoothSendMessage(char *array)
         UARTCharPut(UART3_BASE, *array);
         array++;
     }
+}
+
+void LeerBluetooth(){
+    while (!UARTCharsAvail(UART3_BASE))
+        ;
+    data = UARTCharGetNonBlocking(UART3_BASE);
 }
